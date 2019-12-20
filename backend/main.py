fba54f8b20837b75eb66b70bffce3c49696abf9e
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2019-12-20 15:38:36 +0000

    debug

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from server.router import serve
 
 
 def handler(event, context):
-    return server(path=event, method=event, body=event)
+    return serve(path=event, method=event, body=event)
     return serve(
         path=event.get('path', '').lstrip('/'),
         method=event.get('httpMethod', '').lower(),
