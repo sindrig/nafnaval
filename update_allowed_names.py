c7e73edfdff65d2104fb3e7a98391e8c2c24252d
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2019-12-20 14:19:14 +0000

    allowed names in

---
+++ update_allowed_names.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import os
+import json
+
+import requests
+from bs4 import BeautifulSoup
+
+keymap = {
+    'Drengir': 'male',
+    'Stúlkur': 'female',
+}
+
+base = os.path.dirname(os.path.abspath(__file__))
+result_folder = os.path.join(base, 'backend', 'names')
+
+
+def main():
+    r = requests.get(
+        'https://www.island.is/mannanofn/leit-ad-nafni/',
+        params={'Nafn': '', 'Stulkur': 'on', 'Drengir': 'on'},
+    )
+    r.raise_for_status()
+    soup = BeautifulSoup(r.text, 'html.parser')
+    nametypes = soup.find_all('div', attrs={'class': 'nametype'})
+    for nametype in nametypes:
+        key = keymap[nametype.find('h3').text]
+        names = [i.text.split()[0] for i in nametype.find_all('li')]
+        with open(os.path.join(result_folder, f'{key}.json'), 'w') as f:
+            f.write(
+                json.dumps(names, indent=2, sort_keys=True, ensure_ascii=False)
+            )
+
+
+if __name__ == '__main__':
+    main()
