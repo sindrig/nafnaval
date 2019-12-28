e1d0f1cc13fff57cf2a3e2b187628fa83bef5543
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2019-12-28 10:13:22 +0000

    add language detector back

--- frontend/src/i18n.tsx
+++ frontend/src/i18n.tsx
@@ -1,5 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 const resources = {
   en: {
@@ -34,10 +35,10 @@ const resources = {
 
 i18n
   .use(initReactI18next)
+  .use(LanguageDetector)
   .init({
     resources,
     fallbackLng: 'is',
-    lng: 'is',
     keySeparator: false,
 
     interpolation: {
