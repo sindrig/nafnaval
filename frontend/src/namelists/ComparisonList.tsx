32e6cc797b50e5830bc29d8cb5c83e1911db49f1
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2020-01-01 16:17:02 +0000

    Lint

--- frontend/src/namelists/ComparisonList.tsx
+++ frontend/src/namelists/ComparisonList.tsx
@@ -8,12 +8,9 @@ import { IStoreState } from '../store/reducer';
 import { Progress } from '../store/comparison/types';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-import ShowSelection, { Action } from './ShowSelection';
+import ShowSelection from './ShowSelection';
 
 
 function mapStateToProps({comparison: { names, progress }}: IStoreState) {
