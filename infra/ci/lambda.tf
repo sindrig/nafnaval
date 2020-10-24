feac604acde8f8fac1e9a5f4e9d0d2f8b17e71d5
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2020-10-24 13:35:45 +0000

    Give staging full access of its' own lambda

--- infra/ci/lambda.tf
+++ infra/ci/lambda.tf
@@ -5,11 +5,7 @@ module "lambda-permissions" {
   statements = [
     {
       actions = [
-        "lambda:CreateFunction",
-        "lambda:GetFunction",
-        "lambda:GetFunctionConfiguration",
-        "lambda:UpdateFunctionConfiguration",
-        "lambda:GetAlias",
+        "lambda:*",
       ]
       resources = [
         "arn:aws:lambda:eu-west-1:${data.aws_caller_identity.current.account_id}:function:${data.terraform_remote_state.staging.outputs.lambda_function_name}"
