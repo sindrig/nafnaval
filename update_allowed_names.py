f5eb9876045c7cb7a9b87cf05508dfe89f9cd271
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2021-08-07 21:39:38 +0000

    Fix update allowed names

--- update_allowed_names.py
+++ update_allowed_names.py
@@ -1,35 +1,101 @@
 #!/usr/bin/env python
 import os
+import locale
 import json
 
-import requests
-from bs4 import BeautifulSoup
+import asyncio
+import traceback
+import aiohttp
+
+locale.setlocale(locale.LC_ALL, 'is_IS.utf-8')
 
 keymap = {
-    'Drengir': 'male',
-    'Stúlkur': 'female',
+    'dr': 'male',
+    'st': 'female',
 }
+unused_nametypes = ['mi']
 
 base = os.path.dirname(os.path.abspath(__file__))
 result_folder = os.path.join(base, 'backend', 'server', 'names')
 
 
-def main():
-    r = requests.get(
-        'https://vefur.island.is/mannanofn/leit-ad-nafni/',
-        params={'Nafn': '', 'Stulkur': 'on', 'Drengir': 'on'},
-    )
-    r.raise_for_status()
-    soup = BeautifulSoup(r.text, 'html.parser')
-    nametypes = soup.find_all('div', attrs={'class': 'nametype'})
-    for nametype in nametypes:
-        key = keymap[nametype.find('h3').text]
-        names = [i.text.split()[0] for i in nametype.find_all('li')]
+first_chars = 'aábcdðeéfghiíjklmnoópqrstuúwvxyýzþæö'
+graphql_query = '''query GetIcelandicNameByInitialLetter($input: GetIcelandicNameByInitialLetterInput!) {
+  getIcelandicNameByInitialLetter(input: $input) {
+    id
+    icelandicName
+    type
+    status
+    verdict
+    visible
+    description
+    url
+    __typename
+  }
+  }'''
+
+
+async def fetch_names(session, letter):
+    try:
+        async with session.post(
+            'https://island.is/api/graphql',
+            json=[
+                {
+                    "operationName": "GetIcelandicNameByInitialLetter",
+                    "variables": {"input": {"initialLetter": letter}},
+                    "query": graphql_query,
+                }
+            ],
+        ) as resp:
+            return letter, await resp.json()
+    except Exception as e:
+        traceback.print_exc()
+        raise e
+
+
+async def main():
+    names = {
+        'dr': [],
+        'st': [],
+    }
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for letter in first_chars:
+            task = asyncio.ensure_future(fetch_names(session, letter))
+            tasks.append(task)
+        responses = asyncio.gather(*tasks)
+        for letter, data in await responses:
+            for name in data[0]['data']['getIcelandicNameByInitialLetter']:
+                # Check if name is approved
+                if (name.get('status') or '').lower() == 'sam':
+                    nametype = name['type'].lower()
+                    if nametype.startswith('r'):
+                        # Changed writing, e.g. Agatha and Agata
+                        nametype = nametype.lstrip('r')
+                    if nametype in unused_nametypes:
+                        continue
+                    elif nametype == 'kh':
+                        for nt in names.keys():
+                            names[nt].append(name['icelandicName'])
+                    elif nametype not in names:
+                        print('Unknown nametype ', nametype, name)
+                    else:
+                        names[nametype].append(name['icelandicName'])
+    for nametype, name_list in names.items():
+        key = keymap[nametype]
         with open(os.path.join(result_folder, f'{key}.json'), 'w') as f:
             f.write(
-                json.dumps(names, indent=2, sort_keys=True, ensure_ascii=False)
+                json.dumps(
+                    sorted(
+                        [name.capitalize() for name in name_list],
+                        key=locale.strxfrm,
+                    ),
+                    indent=2,
+                    sort_keys=True,
+                    ensure_ascii=False,
+                )
             )
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
