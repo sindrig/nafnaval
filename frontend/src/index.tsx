ef36aeeaa171f7d2e0372a776dd8c14edfb2478e
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2022-06-08 17:00:39 +0000

    Add user monitoring
    Try to get some grasp or idea about the usage of this thing

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -9,6 +9,29 @@ import * as serviceWorker from './serviceWorker'
 
 import './i18n'
 
+import { AwsRum, AwsRumConfig } from 'aws-rum-web'
+
+try {
+  const config: AwsRumConfig = {
+    sessionSampleRate: 1,
+    guestRoleArn:
+      'arn:aws:iam::525422706348:role/RUM-Monitor-eu-west-1-525422706348-3259537074561-Unauth',
+    identityPoolId: 'eu-west-1:31f7826f-9864-47b9-96a6-fb13dc0b26a3',
+    endpoint: 'https://dataplane.rum.eu-west-1.amazonaws.com',
+    telemetries: ['performance', 'errors', 'http'],
+    allowCookies: true,
+    enableXRay: false,
+  }
+
+  const APPLICATION_ID: string = '6adac9fc-1583-46ad-97eb-139b5dee1ec6'
+  const APPLICATION_VERSION: string = '1.0.0'
+  const APPLICATION_REGION: string = 'eu-west-1'
+
+  new AwsRum(APPLICATION_ID, APPLICATION_VERSION, APPLICATION_REGION, config)
+} catch (error) {
+  // Ignore errors thrown during CloudWatch RUM web client initialization
+}
+
 ReactDOM.render(
   <MuiThemeProvider>
     <Provider store={store}>
